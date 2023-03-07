974c6f90e742d8de6d5f2768a663859aca8e8f3d
TomEdwardo <74116902+TomEdwardo@users.noreply.github.com>
2023-03-07 09:12:20 +0000

    Add files via upload

---
+++ Card.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Card
+    {
+        //Base for the Card class.
+        //Value: numbers 1 - 13
+        //Suit: numbers 1 - 4
+        //The 'set' methods for these properties could have some validation
+        private int _value;
+        private int _suit;
+
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (value > 0 && value < 14)
+                    _value = value;
+            }
+        }
+        public int Suit
+        {
+            get
+            {
+                return _suit;
+            }
+            set
+            {
+                if (value > 0 && value < 5)
+                    _suit = value;
+            }
+        }
+    }
+}
---
+++ Pack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    public class Pack
+    {
+        List<Card> pack;
+
+        public Pack()
+        {
+            //Initialise the card pack here
+            pack = new List<Card>();
+        }
+        public static bool ShuffleCardPack(int typeOfShuffle)
+        {
+            //Shuffles the pack based on the type of shuffle
+            return true;
+        }
+
+        public static List<Card> GetPack()
+        {
+            return pack;
+        }
+
+        public static Card Deal(List<Card> pack)
+        {
+            //Deals one card
+            if (pack.Count > 0)
+            {
+                Card card = pack[0];
+                pack.RemoveAt(0);
+                return card;
+            }
+            return null;
+        }
+        public static List<Card> DealCard(int amount)
+        {
+            //Deals the number of cards specified by 'amount'
+            List<Card> cards = new List<Card>();
+            for (int i = 0; i < amount; i++)
+            {
+                if (pack.Count > 0)
+                {
+                    Card card = pack[0];
+                    pack.RemoveAt(0);
+                    cards.Add
