0ac39a7ee457b4e85cd152d178c3873f3df5fae2
TomEdwardo <74116902+TomEdwardo@users.noreply.github.com>
2023-03-07 09:14:09 +0000

    Add files via upload

--- Card.cs
+++ Card.cs
@@ -12,32 +12,7 @@ namespace CMP1903M_A01_2223
         //Value: numbers 1 - 13
         //Suit: numbers 1 - 4
         //The 'set' methods for these properties could have some validation
-        private int _value;
-        private int _suit;
-
-        public int Value
-        {
-            get
-            {
-                return _value;
-            }
-            set
-            {
-                if (value > 0 && value < 14)
-                    _value = value;
-            }
-        }
-        public int Suit
-        {
-            get
-            {
-                return _suit;
-            }
-            set
-            {
-                if (value > 0 && value < 5)
-                    _suit = value;
-            }
-        }
+        public int Value { get; set; }
+        public int Suit { get; set; }
     }
 }
